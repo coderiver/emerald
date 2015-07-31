898a4eae4335ce1ee1b32b5f51269b2790b93603
moroz <arturmorozv@gmail.com>
2015-07-31 13:20:32 +0300

    without popups for content and gallery

--- js/common.js
+++ js/common.js
@@ -1,5 +1,32 @@
 head.ready(function() {
 
-	
+// slider
+(function () {
+	var sl = $('.js-sl');
+	if (sl.length) {
+		sl.slick({
+			slide: '.js-sl-item'
+		});
+	};
+}());
+
+// popup
+(function () {
+	var body = $('body'),
+		enter = $('.js-popup-enter'),
+		popup = $('.js-popup'),
+		close = $('.js-popup-close');
+	enter.on('click', function () {
+		var el = $(this).data('popup');
+		popup.fadeOut();
+		$('.' + el).fadeIn();
+		body.addClass('no-scroll');
+		return false;
+	});
+	close.on('click', function () {
+		popup.fadeOut();
+		body.removeClass('no-scroll');
+	});
+}());
 	
 });
